f9d06c4ff2c0d2ab5ed07cf66d6d893a1b70968d
jammers <jammers@gmail.com>
2015-03-17 19:05:47 +0300

    reduced calls to route planner

--- include/graph.php
+++ include/graph.php
@@ -17,27 +17,38 @@ function calculate_distance_table($cities){
     $cities2 = array();
     $locations = array();
     $error_cities = array();
-    //Go through each city, compare it to other cities if it's not blank or
-    //comparing against itself
-    foreach($cities as &$i){
-        if($i != ''){
-            $cities2[$i] = array();
-            foreach($cities as $j){
-                if($i != $j && $j != ''){
-                    $x = get_two_cities($i,$j);
-                    $results = parse_city_results($x);
-                    if(!is_null($results)){
-                        $cities2[$i][$j] = $results;
-                        $locations[$i] = $results['start_coord'];
-                        $locations[$j] = $results['end_coord'];
-                    }
-                }
+
+    //remove all the blank cities
+    $cities = array_filter($cities);
+
+    foreach($cities as $city){
+        $cities2[$city] = array();
+    }
+
+    //foreach($cities as &$i){
+    for($i = 0;$i < count($cities);$i++ ){
+        $c1 = $cities[$i];
+
+        for($j = $i+1;$j < count($cities);$j++ ){
+
+            //Get the rsults from the walk between the two
+            $c2 = $cities[$j];
+            $x = get_two_cities($c1,$c2);
+            $results = parse_city_results($x);
+
+            //If it was a real result, put it in both both directions
+            if(!is_null($results)){
+                $cities2[$c1][$c2] = $results;
+                $cities2[$c2][$c1] = $results;
+
+                $locations[$c1] = $results['start_coord'];
+                $locations[$c2] = $results['end_coord'];
             }
-            //If the city couldn't be found it should be removed and added to the list of error cities
-            if(empty($cities2[$i])){
-                unset($cities2[$i]);
-                array_push($error_cities,$i);
-            }
+        }
+        //If the city couldn't be found it should be removed and added to the list of error cities
+        if(empty($cities2[$c1])){
+            unset($cities2[$c1]);
+            array_push($error_cities,$c1);
         }
     }
 
